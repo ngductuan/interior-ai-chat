2098830352b601ab4899566bfccab764f7a93eff
Nguyen Duc Tuan <ngductuan203@gmail.com>
2025-04-02 22:35:16 +0700

    update

---
+++ backend/index.js
@@ -0,0 +1,131 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const os = require("os");
+
+// Create Express app
+const app = express();
+
+// Use bodyParser to parse JSON requests
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+if (process.env.NODE_ENV == "production") {
+  require("dotenv").config({ path: "./.env.production" });
+} else {
+  require("dotenv").config({ path: "./.env" });
+}
+
+console.log(process.env.NODE_ENV);
+
+// Create MySQL connection
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+
+// Connect to MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database: " + err.stack);
+    return;
+  }
+  console.log("Connected to MySQL as ID " + connection.threadId);
+});
+
+// Define the API route to insert a task
+app.post("/task", (req, res) => {
+  const { title } = req.body;
+
+  if (!title) {
+    return res.status(400).json({ error: "Title is required" });
+  }
+
+  // Insert the new task into the task table
+  const query = "INSERT INTO task (title) VALUES (?)";
+  connection.query(query, [title], (err, result) => {
+    if (err) {
+      console.error("Error inserting task: " + err.stack);
+      return res.status(500).json({ error: "Failed to insert task" });
+    }
+
+    res.status(201).json({
+      message: "Task created successfully",
+      taskId: result.insertId,
+    });
+  });
+});
+
+// API route to get all tasks (GET)
+app.get("/tasks", (req, res) => {
+  const query = "SELECT * FROM task";
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error("Error fetching tasks: " + err.stack);
+      return res.status(500).json({ error: "Failed to fetch tasks" });
+    }
+
+    res.status(200).json({
+      message: "Tasks fetched successfully",
+      tasks: results,
+    });
+  });
+});
+
+// API to remove a task (DELETE)
+app.delete("/task/:id", (req, res) => {
+  const taskId = req.params.id;
+
+  // Delete the task from the task table
+  const query = "DELETE FROM task WHERE id = ?";
+  connection.query(query, [taskId], (err, result) => {
+    if (err) {
+      console.error("Error deleting task: " + err.stack);
+      return res.status(500).json({ error: "Failed to delete task" });
+    }
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    res.status(200).json({
+      message: "Task deleted successfully",
+    });
+  });
+});
+
+// API route to get all tasks (GET)
+app.get("/", (req, res) => {
+  const clientIp = req.header("x-forwarded-for");
+  const elbIP = req.socket.remoteAddress;
+  const containerIp = req.socket.localAddress;
+  const containerName = os.hostname();
+
+  console.log("Hello Amazon ECS!");
+
+  res.status(200).json({
+    serviceName: "ECS User Microservice",
+    clientIp: clientIp,
+    elbIP: elbIP,
+    containerIp: containerIp,
+    containerName: containerName,
+  });
+});
+
+// Start the Express server
+const port = process.env.HTTP_PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ frontend/src/App.vue
@@ -0,0 +1,169 @@
+<template>
+  <div id="app">
+    <h1>Todo App</h1>
+
+    <div id="task-form">
+      <input v-model="newTask" type="text" placeholder="Enter task title" />
+      <button @click="createTask">Add Task</button>
+    </div>
+
+    <h2>Task List</h2>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.title }}
+        <button @click="deleteTask(task.id)" class="delete-btn">🗑</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+export default {
+  name: "App",
+  setup() {
+    const newTask = ref("");
+    const tasks = ref([]);
+
+    const apiUrl = import.meta.env.VITE_API_URL; // Use the environment variable
+
+    // Fetch tasks when the app is mounted
+    const fetchTasks = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/tasks`);
+        tasks.value = response.data.tasks;
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+      }
+    };
+
+    // Create a new task
+    const createTask = async () => {
+      if (!newTask.value) {
+        alert("Please enter a task title");
+        return;
+      }
+
+      try {
+        const response = await axios.post(`${apiUrl}/task`, {
+          title: newTask.value,
+        });
+        console.log(response.data.message);
+        fetchTasks(); // Refresh the task list
+      } catch (error) {
+        console.error("Error creating task:", error);
+      }
+
+      newTask.value = ""; // Clear the input field
+    };
+
+    // Delete a task
+    const deleteTask = async (taskId) => {
+      try {
+        const response = await axios.delete(`${apiUrl}/task/${taskId}`);
+        console.log(response.data.message);
+        fetchTasks(); // Refresh the task list after deleting
+      } catch (error) {
+        console.error("Error deleting task:", error);
+      }
+    };
+
+    // Fetch tasks on component mount
+    onMounted(fetchTasks);
+
+    return { newTask, tasks, createTask, deleteTask };
+  },
+};
+</script>
+
+<style scoped>
+body {
+  font-family: "Arial", sans-serif;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f0f4f8;
+}
+
+#app {
+  text-align: center;
+  background-color: #ffffff;
+  padding: 30px;
+  border-radius: 15px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  font-size: 36px;
+  color: #4a90e2;
+  margin-bottom: 20px;
+}
+
+#task-form {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+}
+
+input {
+  padding: 15px;
+  font-size: 18px;
+  /* width: 70%; */
+  width: 300px;
+  margin-right: 15px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  background-color: lightblue;
+  color: black;
+}
+
+#task-form button {
+  padding: 15px 40px;
+  font-size: 16px;
+  background-color: #5cb85c;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  white-space: nowrap;
+}
+
+#task-form button:hover {
+  background-color: #4cae4c;
+}
+
+h2 {
+  font-size: 24px;
+  color: #333;
+  margin-bottom: 20px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  padding: 12px 20px;
+  font-size: 18px;
+  background-color: #f9f9f9;
+  margin: 10px 0;
+  border-radius: 8px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: darkorange;
+}
+
+li button {
+  background-color: lightgray;
+}
+</style>
